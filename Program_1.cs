0605b23716e542f379317e19b9f143d797c23f71
trib3rg <121116935+trib3rg@users.noreply.github.com>
2022-12-25 17:20:09 +0300

    Add files via upload
    A program, which counts arithmetic average of three numbers, typed in one line with spaces, using Split method.

---
+++ Program_1.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IdkStudying_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double result;
+            string variables;
+            Console.WriteLine("Программа предназначена для нахождения среднего арифметического трёх чисел.\nВведите три переменные через пробел:");
+            variables = Console.ReadLine();
+            string[] numbers = variables.Split(' ');
+            result = (Double.Parse(numbers[0]) + Double.Parse(numbers[1]) + Double.Parse(numbers[2])) / 3;
+            Console.WriteLine("\nРезультат = " + result);
+        }
+    }
+}
